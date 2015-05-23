c0ec19c41069ea90cc0ec1a8ea87a11594831567
Remco Tolsma <info@remcotolsma.nl>
2015-05-23 14:31:49 +0200

    Renamed the functions to the new namespaced classes.

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function pronamic_ideal_wpsc_merchant_form() {
-	return Pronamic_WPeCommerce_IDeal_IDealMerchant::admin_config_form();
+	return Pronamic_WP_Pay_Extensions_WPeCommerce_IDealMerchant::admin_config_form();
 }
 
 function pronamic_ideal_wpsc_merchant_submit_function() {
-	return Pronamic_WPeCommerce_IDeal_IDealMerchant::admin_config_submit();
+	return Pronamic_WP_Pay_Extensions_WPeCommerce_IDealMerchant::admin_config_submit();
 }
