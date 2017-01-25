f85fd2a2039f339b1b04bcfe0fbc81676637c784
Remco Tolsma <info@remcotolsma.nl>
2017-01-25 14:55:17 +0100

    Getting ready for version 1.0.4.

--- src/Extension.php
+++ src/Extension.php
@@ -3,11 +3,11 @@
 /**
  * Title: WP eCommerce extension
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.4
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_WPeCommerce_Extension {
